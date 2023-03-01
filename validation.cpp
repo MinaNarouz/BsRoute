4b745a9a0ec88477a3f83837bfb6aadb25a0c801
Alaa360 <55282078+Alaa360@users.noreply.github.com>
2023-03-01 13:22:32 +0200

    Created Validation class

---
+++ validation.cpp
@@ -0,0 +1,12 @@
+#include"validation.h"
+
+class Validation {
+    static bool checkName(string name) {}
+    static bool checkPassword(string password) {}
+public:
+    static string insertName() {}
+    static string insertPassword() {}
+
+    static double insertBalance() {}
+    static double insertSalary() {}
+};
